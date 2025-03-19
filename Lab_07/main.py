1fa1494d3b16cfde71c2d4d6633bfeca082c83bb
Faraz Ul Hassan <farazulhassan0425@gmail.com>
2025-03-19 23:06:48 +0500

    Add files via upload

---
+++ Lab_07/main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+import requests # type: ignore
+
+app = Flask(__name__)
+
+NASA_API_KEY = "4nLgHaD70SbVxeghCfczPHG2G9U2hT0ld56TiKES"
+#Endpoints
+apod_url = "https://api.nasa.gov/planetary/apod"
+
+@app.route("/",methods=["GET"])
+def index():
+    #Fetching Data From NASA
+    params = {"api_key":NASA_API_KEY}
+    response = requests.get(apod_url,params=params)
+    apod_data = response.json()
+    return render_template("index.html",apod = apod_data)
+
+if __name__ == "__main__":
+    app.run(debug=False)
